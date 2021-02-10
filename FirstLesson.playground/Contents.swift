f5b079b7d9df16b2f72c986d4e7f6122fe3ae819
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2021-02-10 11:43:53 +0100

    FirstLesson

---
+++ FirstLesson.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit //libreria per la UI
+
+var str = "Hello, playground" //non ha punto e virgola e non ha tipo esplicito ma dato che è fortemente tipizzato usa la tipizzzazione automatica, safe ?enserection?.
+
+let pippo = 2 //let è la dichiarazione di una costante, metre var una variabile; consuma meno memoria di una variabile che per definizione risulta essere mutabile a differenza di una const che è immutabile.
+
+print(str)
+print(pippo)
